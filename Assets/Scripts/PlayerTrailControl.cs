ada51ceec361e401acf8304b3923cc9491cb0943
NocaWu <noca@nyu.edu>
2017-04-07 23:59:00 -0400

    particles

---
+++ Assets/Scripts/PlayerTrailControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrailControl : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("PlayerShell");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position;// - new Vector3 (0,0.1f,0);
+	}
+}
