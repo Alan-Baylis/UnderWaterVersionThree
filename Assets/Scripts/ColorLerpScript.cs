7db16190cf84f5bf8a503e173103cf8c0e8a293f
NocaWu <noca@nyu.edu>
2017-04-13 12:22:23 -0400

    title screen in progress

---
+++ Assets/Scripts/ColorLerpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorLerpScript : MonoBehaviour {
+
+	public Color colorOri;
+	public Color colorTarget;
+	public Color colorCurrent;
+
+	// Use this for initialization
+	void Start () {
+		colorCurrent = Color.clear;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		colorCurrent = Color.LerpUnclamped(colorOri,colorTarget,Mathf.Sin(Time.time));
+		gameObject.GetComponent<Text> ().color = colorCurrent;
+	}
+}
