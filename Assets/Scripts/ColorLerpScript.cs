8572edfed4a4a82f1eecfae10b8ce992079ed3e0
NocaWu <noca@nyu.edu>
2017-04-13 13:07:28 -0400

    adjust cam pos on title screen

--- Assets/Scripts/ColorLerpScript.cs
+++ Assets/Scripts/ColorLerpScript.cs
@@ -9,15 +9,19 @@ public class ColorLerpScript : MonoBehaviour {
 	public Color colorTarget;
 	public Color colorCurrent;
 
+	public float scaleMin;
+	private Vector3 scaleOri;
+
 	// Use this for initialization
 	void Start () {
 		colorCurrent = Color.clear;
-
+		scaleOri = GetComponent<RectTransform> ().localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		colorCurrent = Color.LerpUnclamped(colorOri,colorTarget,Mathf.Sin(Time.time));
 		gameObject.GetComponent<Text> ().color = colorCurrent;
+		GetComponent<RectTransform> ().localScale = Vector3.LerpUnclamped (scaleOri,scaleOri * scaleMin,Mathf.Sin(Time.time));
 	}
 }
