76a5a063469566ea5ccfa1d0f755a71cf3d871a7
NocaWu <noca@nyu.edu>
2017-04-28 21:22:42 -0400

    tuned jellymesh

--- Assets/Scripts/CubeControl.cs
+++ Assets/Scripts/CubeControl.cs
@@ -9,36 +9,15 @@ public class CubeControl : MonoBehaviour {
 	void Start () 
 	{
 		posOri = transform.position;
-//		Debug.Log (posOri);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-//		// raycasting way of floating
-//		Ray floatRay = new Ray(transform.position, Vector3.down * 10);
-//		RaycastHit floatRayhit;
-//
-//		if (Physics.Raycast (floatRay, out floatRayhit, 10f)) {
-//			// get the game object's y, change itself to make itself float
-//			if (floatRayhit.collider.gameObject.tag == "GroundCube"){
-//				Vector3 groundCubePos;
-//				Vector3 temp = transform.position;
-//				groundCubePos = floatRayhit.collider.gameObject.transform.position;
-//				transform.position = Vector3.Lerp (temp,new Vector3 (temp.x, groundCubePos.y + 2.2f, temp.z),0.1f);
-//			}
-//		}
-
 		heightAdjust = new Vector3 (
 			0.0f, 
-			Mathf.Sin (Time.time - transform.position.x * 0.2f - transform.position.z * 0.2f), 
+			GroundSinControl.CalculateSinPosition(transform.position),
 			0.0f);
-
-//		transform.RotateAround (
-//			posOri, 
-//			Vector3.forward,
-//			2.5f * Time.deltaTime * Mathf.Cos (Time.time)
-//		);
 
 		transform.Rotate (new Vector3 (45, 15, 30) * Time.deltaTime);
 		transform.position = posOri + heightAdjust;// + new Vector3(0,2.2f,0);
